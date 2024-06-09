0fe4686695a1d6938f7139d92fa7fd7879b27daf
Aleksandra Malinowska <amalinowska423@gmail.com>
2024-06-09 16:36:30 +0200

    v15

---
+++ v15/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TestsService} from "./tests.service";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule],
+  providers: [TestsService],
+  template: `
+    <h1>Angular {{testsService.version}} Performance Tests</h1>
+    <div style="display: flex; flex-wrap: wrap; gap: 12px; margin-bottom: 24px">
+      <button *ngFor="let simulation of testsService.simulations$ | async" (click)="simulation.fn()">
+        Run {{ simulation.name }} Simulation
+      </button>
+      <button (click)="testsService.downloadResultsCSV()">Download results CSV</button>
+      <button (click)="testsService.clearResults()">Clear results</button>
+    </div>
+
+    <p *ngFor="let user of testsService.users$ | async">
+      <span>{{ user.id }}</span> -
+      <span>{{ user.name }}</span> -
+      <span>{{ user.phone }}</span> -
+      <span>{{ user.email }}</span> -
+      <span>{{ user.city }}</span> -
+      <span>{{ user.country }}</span> -
+      <span>{{ user.is_active }}</span>
+    </p>
+  `
+})
+export class AppComponent {
+  constructor(public testsService: TestsService) {
+  }
+}
---
+++ v15/src/app/app.config.ts
@@ -0,0 +1,8 @@
+import {provideRouter} from '@angular/router';
+
+import {routes} from './app.routes';
+import {ApplicationConfig} from "@angular/platform-browser";
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes)]
+};
