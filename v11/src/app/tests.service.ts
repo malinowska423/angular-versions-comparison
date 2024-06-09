90ae6c40e9c7704057853acc768f0e9a213a9f42
Aleksandra Malinowska <amalinowska423@gmail.com>
2024-06-09 17:47:15 +0200

    v11

---
+++ v11/src/app/tests.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from "@angular/core";
+import {User} from "../../../common/user.model";
+import {AbstractTestsService} from "../../../common/tests.service.abstract";
+import {BehaviorSubject, Observable, of} from "rxjs";
+
+
+@Injectable()
+export class TestsService extends AbstractTestsService {
+  readonly version: string = 'v11';
+
+  private readonly _users$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+  readonly users$: Observable<User[]> = this._users$.asObservable();
+
+  readonly simulations$ = of(this._simulations);
+
+  protected set data(value: User[]) {
+    this._users$.next([...value]);
+  }
+
+  protected get data(): User[] {
+    return this._users$.value;
+  }
+}
